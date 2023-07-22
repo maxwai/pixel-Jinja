9ad877fa7d72a3b933c8fbfd58f8cd5284aeca98
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-07-22 10:52:16 +0200

    morning fixes

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python pixel.py ./pictures ./target_config.toml --config ";;./outputs/overlay_target.png;"";"";"";1;0;1;1"
+python pixel.py ./pictures ./target_config.toml --config ";;./outputs/overlay_target.png;"";"";"";1;0;1;1" --config ";;./outputs/artworks_target.png;"";"";"";0;0;1;1"
